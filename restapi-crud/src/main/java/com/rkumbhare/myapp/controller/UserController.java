cab2d2263497f9ff53e5dd8570b7d405b955ede1
Rakesh <Rakesh@rakesh-pc>
2017-02-12 11:50:22 +0530

    configure userController as RestController with base url '/user'

--- restapi-crud/src/main/java/com/rkumbhare/myapp/controller/UserController.java
+++ restapi-crud/src/main/java/com/rkumbhare/myapp/controller/UserController.java
@@ -5,6 +5,11 @@
 
 package com.rkumbhare.myapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
 }
